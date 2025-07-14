57fd52708fbee80717d600b023b4d08f14f0267a
cdilsiz5 <cihandilsizdev@gmail.com>
2025-07-14 13:49:51 +0300

    Web Configurations are added.

--- src/main/java/com/epam/springcore/config/AppConfig.java
+++ src/main/java/com/epam/springcore/config/AppConfig.java
@@ -1,11 +1,28 @@
 package com.epam.springcore.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = "com.epam.springcore")
 @PropertySource("classpath:application.properties")
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule()); // LocalDate, LocalDateTime için
+        converters.add(new MappingJackson2HttpMessageConverter(mapper));
+    }
+
 }
